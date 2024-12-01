2d701acea5b691c2bdd243a92dd73e002c9e80dd
Champu <117635421+TheChampu@users.noreply.github.com>
2024-12-01 16:32:18 +0530

    Try

---
+++ ChampuMusic/__init__.py
@@ -0,0 +1,27 @@
+from ChampuMusic.core.bot import Champu
+from ChampuMusic.core.dir import dirr
+from ChampuMusic.core.git import git
+from ChampuMusic.core.userbot import Userbot
+from ChampuMusic.misc import dbb, heroku
+from pyrogram import Client
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Champu()
+api = SafoneAPI()
+userbot = Userbot()
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
